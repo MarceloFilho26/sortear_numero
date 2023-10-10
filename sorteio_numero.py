d1dcecd2907f5dd31b8edadd21b85d4bcf5c9c58
Marcelo Bruno <113138209+MarceloFilho26@users.noreply.github.com>
2023-10-10 08:16:23 -0300

    sorteio de número utilizando python

--- sorteio_numero.py
+++ sorteio_numero.py
@@ -12,3 +12,6 @@ while resp == "S":
     while (resp != "S" and resp !="s") and (resp != "N" and resp !="n"):
         resp = input("Opção inválida, escolha novamente [S/N]: ")
 print("Programa encerrado!")
+
+#O programa pede ao usuário quantos número ele deseja sortear e também solicita a ele qual o número ele irá até onde vai;
+#No fim ele ainda pergunta se deseja refazer o sorteio ou encerrar o programa
