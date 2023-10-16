e678162c502530e7747e64df5cb835bf46ab8f1d
TURMA-A <mbsnfilho22@gmail.com>
2023-10-16 07:34:44 -0300

    sorteio de número utilizando python

--- sorteio_numero.py
+++ sorteio_numero.py
@@ -3,10 +3,8 @@ resp = "S"
 
 while resp == "S":
     n = int(input("Informe a quantidade de números: "))
-    n1 = int(input("Informe o número inicial: "))
-    n2 = int(input("Informe até qual número irá: "))
     for i in range(n):
-        numero_sorteado = random.randint(n1, n2)
+        numero_sorteado = random.randint(1, 26)
         print(numero_sorteado)
     resp = input("Deseja sortear novamemte? [S/N]: ")
     while (resp != "S" and resp !="s") and (resp != "N" and resp !="n"):
